9665b275ceb06aa3f3fc245dff87d62819f0c8b4
Mohammad Amaan Sharif <97755687+amaan-272@users.noreply.github.com>
2022-12-29 22:04:01 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './PathFindingVisualizer/PathFindingVisualizer';
+import PathfindingVisualizer from './PathFindingVisualizer/PathFindingVisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
